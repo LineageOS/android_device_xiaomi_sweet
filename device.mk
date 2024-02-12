b4eec28a20b87db517cef3489f463da7edc5bb8e
LuK1337 <priv.luk@gmail.com>
2024-02-12 12:58:30 +0100

    sweet: Redo libsdmcore shim
    Instead of injecting custom dependency into libsdmcore.so, provide a
    custom hwcomposer implementation that overrides the *unwanted* symbol
    before loading the vendor implementation.
    
    Change-Id: Icc6551f9260a0eb8a7f7a616588c65472a9366e9

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml
 
 PRODUCT_PACKAGES += \
-    libsdmcore-shim
+    hwcomposer.qcom
 
 # Init scripts
 PRODUCT_COPY_FILES += \
