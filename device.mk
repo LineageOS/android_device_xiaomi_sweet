287b747356db935e8ce138286bea487760628a5f
H. Felix Wittmann <hfwittmann@hfwittmann-HVY-WXX9.speedport.ip>
2026-07-29 18:06:17 +0200

    sweet: nfc: Enable ISO14443-4 extended APDU support
    Enable ISO14443_4_EXTENDED_LENGTH=0x01 in libnfc-nxp.conf to allow
    transceiving extended APDU frames required for smart card and eID authentication.
    
    Base file sourced from stock Xiaomi vendor configuration:
    https://github.com/TheMuppets/proprietary_vendor_xiaomi_sweet/blob/lineage-23.2/proprietary/vendor/etc/libnfc-nxp.conf
    
    Change-Id: Icb65dfa694df26143a58c3e3ce3fd9820b40b92b

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_sweet/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_sweet/com.nxp.mifare.xml
 
+# NFC config override for ISO14443-4 extended APDU support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.lineage
